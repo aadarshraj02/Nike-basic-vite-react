6eda11650fd024511fc67072891ee7f67f9f852f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-17 11:24:29 +0530

    added 2 div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Hero() {
-  return <div></div>;
+  return (
+    <div>
+      <div className="text"></div>
+      <div className="image"></div>
+    </div>
+  );
 }
 
 export default Hero;
