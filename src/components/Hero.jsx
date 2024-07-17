e8a74ba19c073ed4a5ca4e16117b300ea552dad1
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-17 11:51:18 +0530

    added styling to button div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,14 @@ function Hero() {
   return (
     <div className="flex mx-auto max-w-[1280px] items-center h-[90vh]">
       <div className="text">
-        <h1 className="font-bold text-[8vw] leading-none tracking-tight">YOUR FEET DESERVE <br /> THE BEST</h1>
+        <h1 className="font-bold text-[8vw] leading-none tracking-tight">
+          YOUR FEET DESERVE <br /> THE BEST
+        </h1>
         <p className="w-1/2 mt-3 text-justify text-[1vw]">
           YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
           YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
         </p>
-        <div className="buttons">
+        <div className="mt-3 flex items-center justify-around">
           <button>Shop Now</button>
           <button>Category</button>
         </div>
