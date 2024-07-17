27d3a9556a122d9b363c7ffb36dd1b1b51562f19
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-17 11:17:08 +0530

    removed nav code

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
-      <nav className="flex items-center justify-between max-h-[72px] max-w-[1280px] mx-auto ">
-        <div className="logo">
-          <img src="../public/brand_logo.png" alt="" />
-        </div>
-        <ul className="flex gap-10 items-center">
-          <li>Menu</li>
-          <li>Location</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-        <button className="bg-red-600 text-white px-5 py-2 rounded-md hover:opacity-80">
-          Login
-        </button>
-      </nav>
+      
     </div>
   );
 }
