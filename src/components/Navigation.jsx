c5018005c1e037e0272cb510f0a54d0d93528b3d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-17 11:17:28 +0530

    added nav code

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 function Navigation() {
-  return <div></div>;
+  return (
+    <div>
+      <nav className="flex items-center justify-between max-h-[72px] max-w-[1280px] mx-auto ">
+        <div className="logo">
+          <img src="../public/brand_logo.png" alt="" />
+        </div>
+        <ul className="flex gap-10 items-center">
+          <li>Menu</li>
+          <li>Location</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <button className="bg-red-600 text-white px-5 py-2 rounded-md hover:opacity-80">
+          Login
+        </button>
+      </nav>
+    </div>
+  );
 }
 
 export default Navigation;
