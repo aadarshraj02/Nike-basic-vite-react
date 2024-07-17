35b4f3ebeabaeca331b6e639cffb0fa63949ffd1
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-17 09:32:27 +0530

    moved images to public

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 
 function App() {
-  return <div>
-    <nav>
-      <div className="logos">
-
-      </div>
-    </nav>
-  </div>;
+  return (
+    <div>
+      <nav>
+        <div className="logo">
+          <img src="./assets/brand_logo.png" alt="" />
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default App;
