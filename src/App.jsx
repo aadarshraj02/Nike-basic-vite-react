2a5770920e85a5cbe93cf66384126ee1c72cfe7b
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-17 11:22:28 +0530

    imported hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
 
 function App() {
   return (
     <div>
       <Navigation></Navigation>
+      <Hero></Hero>
     </div>
   );
 }
