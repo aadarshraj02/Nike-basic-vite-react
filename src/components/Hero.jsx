f654c34ed141596d9c992ba3f54bf403613e8c11
aadarshraj02 <aadarshraazz007@gmail.com>
2024-07-17 11:26:50 +0530

    added h1 and p

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function Hero() {
   return (
     <div>
-      <div className="text"></div>
+      <div className="text">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE ARE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
+      </div>
       <div className="image"></div>
     </div>
   );
